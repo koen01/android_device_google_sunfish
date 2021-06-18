f1840eb961816341bacbfbbc118a79a3815b9c3b
koen01 <koen.erens@gmail.com>
2021-06-18 13:45:32 +0200

    Revert "sunfish: Build LiveDisplay 2.0 HAL"
    This reverts commit 7566d57a842285ed6c14b9c3b943a8fc450cd6ec.

--- device-lineage.mk
+++ device-lineage.mk
@@ -26,10 +26,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.pixel \
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
